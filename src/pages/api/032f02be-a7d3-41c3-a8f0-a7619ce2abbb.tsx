4b824516586f63dfa729357de053db75c5032b4a
stryder03 <stryder03@users.noreply.github.com>
2020-08-01 14:16:32 -0600

    fix(auth): added correct params

--- src/pages/api/032f02be-a7d3-41c3-a8f0-a7619ce2abbb.tsx
+++ src/pages/api/032f02be-a7d3-41c3-a8f0-a7619ce2abbb.tsx
@@ -1,27 +1,15 @@
-import * as admin from 'firebase-admin'
-import * as Sentry from "@sentry/node"
+import {verifyIdToken} from "src/utils/auth/firebaseAdmin";
 
-Sentry.init({ dsn: process.env.SENTRY_DSN})
+const fbRes = async (req: any, res: any) => {
 
-export const verifyIdToken = (token: string) => {
-    const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY
-
-    if (!admin.apps.length && firebasePrivateKey) {
-        admin.initializeApp({
-            credential: admin.credential.cert({
-                projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-                // https://stackoverflow.com/a/41044630/1332513
-                privateKey: firebasePrivateKey.replace(/\\n/g, '\n'),
-            }),
-            databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-        })
+    const token = req.headers.token
+    try {
+        await verifyIdToken(token)
+        res.send("token valid");
     }
-
-    return admin
-        .auth()
-        .verifyIdToken(token)
-        .catch((error) => {
-            throw error
-        })
+    catch (error) {
+        return res.status(401).send('You are unauthorised')
+    }
 }
+
+export default fbRes;
---
+++ src/utils/auth/firebaseAdmin.tsx
@@ -0,0 +1,25 @@
+import * as admin from 'firebase-admin'
+
+export const verifyIdToken = (token: string) => {
+    const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY
+
+    if (!admin.apps.length && firebasePrivateKey) {
+        admin.initializeApp({
+            credential: admin.credential.cert({
+                projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+                // https://stackoverflow.com/a/41044630/1332513
+                privateKey: firebasePrivateKey.replace(/\\n/g, '\n'),
+            }),
+            databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+        })
+    }
+    console.log("token: " + token)
+    return admin
+        .auth()
+        .verifyIdToken(token)
+        .catch((error) => {
+            console.log(error)
+            throw error
+        })
+}
